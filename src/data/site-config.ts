b1c3e7a60430fb417ff6249ad1211d72abd2892d
sklasfeld <sjk314@gmail.com>
2025-11-13 21:32:56 -0500

    add source code to footer

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -1,20 +1,20 @@
 export type Image = {
-    src: string;
-    alt?: string;
-    caption?: string;
+  src: string;
+  alt?: string;
+  caption?: string;
 };
 
 export type Link = {
-    text: string;
-    href: string;
-    download?: string;
+  text: string;
+  href: string;
+  download?: string;
 };
 
 export type Hero = {
-    title?: string;
-    text?: string;
-    image?: Image;
-    actions?: Link[];
+  title?: string;
+  text?: string;
+  image?: Image;
+  actions?: Link[];
 };
 /*
 export type Subscribe = {
@@ -25,111 +25,111 @@ export type Subscribe = {
 */
 
 export type SiteConfig = {
-    logo?: Image;
-    title?: string;
-    subtitle?: string;
-    description?: string;
-    image?: Image;
-    headerNavLinks?: Link[];
-    footerNavLinks?: Link[];
-    socialLinks?: Link[];
-    hero?: Hero;
-    /*subscribe?: Subscribe;*/
-    postsPerPage?: number;
-    projectsPerPage?: number;
+  logo?: Image;
+  title?: string;
+  subtitle?: string;
+  description?: string;
+  image?: Image;
+  headerNavLinks?: Link[];
+  footerNavLinks?: Link[];
+  socialLinks?: Link[];
+  hero?: Hero;
+  /*subscribe?: Subscribe;*/
+  postsPerPage?: number;
+  projectsPerPage?: number;
 };
 
 const siteConfig: SiteConfig = {
-    title: "Samantha Klasfeld",
-    subtitle: "Computational Biologist, Bioinformatics Scientist",
-    description: 'Samantha Klasfeld\'s Professional Portfolio',
-    image: {
-        src: '/favicon.png',
-        alt: 'Samantha Klasfeld logo'
+  title: 'Samantha Klasfeld',
+  subtitle: 'Computational Biologist, Bioinformatics Scientist',
+  description: "Samantha Klasfeld's Professional Portfolio",
+  image: {
+    src: '/favicon.png',
+    alt: 'Samantha Klasfeld logo',
+  },
+  headerNavLinks: [
+    {
+      text: 'Home',
+      href: '/',
     },
-    headerNavLinks: [
-        {
-            text: 'Home',
-            href: '/'
-        },
-        {
-            text: 'Experiences',
-            href: '/projects'
-        },
-        {
-            text: 'Contact',
-            href: '/contact'
-        }
-        /*,
+    {
+      text: 'Experiences',
+      href: '/projects',
+    },
+    {
+      text: 'Contact',
+      href: '/contact',
+    },
+    /*,
         {
             text: 'Tags',
             href: '/tags'
         }*/
-    ],
-    footerNavLinks: [
-        /*{
+  ],
+  footerNavLinks: [
+    /*{
             text: 'About',
             href: '/about'
         },*/
-        {
-            text: 'Contact',
-            href: '/contact'
-        },/*
+    {
+      text: 'Contact',
+      href: '/contact',
+    } /*
         {
             text: 'Terms',
             href: '/terms'
-        }*/
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
-        }
-    ],
-    socialLinks: [
-        {
-            text: 'GitHub',
-            href: 'https://github.com/sklasfeld'
-        },
-        {
-            text: 'Kaggle',
-            href: 'https://www.kaggle.com/sklasfeld'
-        },
-        {
-            text: 'LinkedIn',
-            href: 'https://www.linkedin.com/in/samantha-klasfeld/'
-        },
-        {
-            text: 'BlueSky',
-            href: 'https://bsky.app/profile/sklasfeld.bsky.social'
-        }
-    ],
-    hero: {
-        title: 'About Me',
-        text: "A computational biologist with expertise in statistical genetics \
+        }*/,
+    {
+      text: 'Source Code',
+      href: 'https://github.com/sklasfeld/sklasfeld.github.io',
+    },
+  ],
+  socialLinks: [
+    {
+      text: 'GitHub',
+      href: 'https://github.com/sklasfeld',
+    },
+    {
+      text: 'Kaggle',
+      href: 'https://www.kaggle.com/sklasfeld',
+    },
+    {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/samantha-klasfeld/',
+    },
+    {
+      text: 'BlueSky',
+      href: 'https://bsky.app/profile/sklasfeld.bsky.social',
+    },
+  ],
+  hero: {
+    title: 'About Me',
+    text: "A computational biologist with expertise in statistical genetics \
         and genomics analysis, I am committed to solving complex questions through \
         data analysis, statistical inference, collaboration, and creativity.<br><br>\
         I am currently working as an Independent Contractor at Genscience. \
         <div class='underline'> \
         [Check out my postdoc project in Human Genetics and Genomics Advances](https://www.cell.com/hgg-advances/fulltext/S2666-2477(25)00063-6)! \
         </div>",
-        image: {
-            src: '/sklasfeld_cartoon.jpg',
-            alt: 'Professional Photo of Samantha Klasfeld'
-        },
-        actions: [
-            {
-                text: 'Download Resume',
-                href: 'Samantha_Klasfeld_CV.pdf',
-                download: 'Samantha_Klasfeld_CV.pdf'
-            }
-        ]
+    image: {
+      src: '/sklasfeld_cartoon.jpg',
+      alt: 'Professional Photo of Samantha Klasfeld',
     },
-    /*subscribe: {
+    actions: [
+      {
+        text: 'Download Resume',
+        href: 'Samantha_Klasfeld_CV.pdf',
+        download: 'Samantha_Klasfeld_CV.pdf',
+      },
+    ],
+  },
+  /*subscribe: {
         title: 'Subscribe to Dante Newsletter',
         text: 'One update per week. All the latest posts directly in your inbox.',
         formUrl: '#'
     },*/
-    postsPerPage: 8,
-    projectsPerPage: 8
+  postsPerPage: 8,
+  projectsPerPage: 8,
 };
 
 export default siteConfig;
